ff5059f08f57561b0fa4058ffe0c77034af90302
katao <k.katasan@gmail.com>
2023-06-05 12:19:57 +0900

    スコア管理用GUIを登録

--- manage_score.py
+++ manage_score.py
@@ -2,9 +2,12 @@
 # GUI機能も有しており、本ソース単体でも動作する
 import pickle
 import datetime
-import os, sys
+import json
+import csv
+import sys, os
 from collections import defaultdict
-#import PySimpleGUI as sg
+import PySimpleGUI as sg
+from tkinter import filedialog
 
 class ScoreManager:
     def __init__(self):
@@ -46,9 +49,9 @@ class ScoreManager:
     def get_scores_best(self):
         lamp_table = ['NO PLAY', 'FAILED', 'A-CLEAR', 'E-CLEAR', 'CLEAR', 'H-CLEAR', 'EXH-CLEAR', 'F-COMBO']
         for key in self.score.keys():
-            # ランプ、スコア、BP、best_opt_score, best_opt_bp、最終プレー日
-            best_reg = ['NO PLAY', 0, 9999, '?', '?', '2000-01-01'] 
-            best_dbx = ['NO PLAY', 0, 9999, '?', '?', '2000-01-01'] 
+            # ランプ、スコア、BP、notes, best_opt_score, best_opt_bp、最終プレー日
+            best_reg = ['NO PLAY', 0, 9999, 0, '?', '?', '2000-01-01'] 
+            best_dbx = ['NO PLAY', 0, 9999, 0, '?', '?', '2000-01-01'] 
             for s in self.score[key]: # その曲の全ログを確認
                 tmp = list(map(int, s[-1].split('-')))
                 date_cur = datetime.date(tmp[0], tmp[1], tmp[2])
@@ -60,11 +63,12 @@ class ScoreManager:
                     if s[9] != None:
                         if best_dbx[1] < s[9]: # スコア
                             best_dbx[1] = s[9]
-                            best_dbx[3] = s[-2]
+                            best_dbx[4] = s[-2]
+                            best_dbx[3] = s[3]
                     if s[11] != None:
                         if best_dbx[2] > s[11]: # bp
                             best_dbx[2] = s[11]
-                            best_dbx[4] = s[-2]
+                            best_dbx[5] = s[-2]
                     if s[7] != None:
                         if lamp_table.index(best_dbx[0]) < lamp_table.index(s[7]):
                             best_dbx[0] = s[7]
@@ -76,11 +80,12 @@ class ScoreManager:
                     if s[9] != None:
                         if best_reg[1] < s[9]: # スコア
                             best_reg[1] = s[9]
-                            best_reg[3] = s[-2]
+                            best_reg[4] = s[-2]
+                            best_reg[3] = s[3]
                     if s[11] != None:
                         if best_reg[2] > s[11]: # bp
                             best_reg[2] = s[11]
-                            best_reg[4] = s[-2]
+                            best_reg[5] = s[-2]
                     if s[7] != None:
                         if lamp_table.index(best_reg[0]) < lamp_table.index(s[7]):
                             best_reg[0] = s[7]
@@ -90,11 +95,12 @@ class ScoreManager:
                     if s[8] != None:
                         if best_dbx[1] < s[8]: # スコア
                             best_dbx[1] = s[8]
-                            best_dbx[3] = '?'
+                            best_dbx[4] = '?'
+                            best_dbx[3] = s[3]
                     if s[10] != None:
                         if best_dbx[2] > s[10]: # bp
                             best_dbx[2] = s[10]
-                            best_dbx[4] = '?'
+                            best_dbx[5] = '?'
                     if s[6] != None:
                         if lamp_table.index(best_dbx[0]) < lamp_table.index(s[6]):
                             best_dbx[0] = s[6]
@@ -102,11 +108,12 @@ class ScoreManager:
                     if s[8] != None:
                         if best_reg[1] < s[8]: # スコア
                             best_reg[1] = s[8]
-                            best_reg[3] = '?'
+                            best_reg[4] = '?'
+                            best_reg[3] = s[3]
                     if s[10] != None:
                         if best_reg[2] > s[10]: # bp
                             best_reg[2] = s[10]
-                            best_reg[4] = '?'
+                            best_reg[5] = '?'
                     if s[6] != None:
                         if lamp_table.index(best_reg[0]) < lamp_table.index(s[6]):
                             best_reg[0] = s[6]
@@ -159,7 +166,127 @@ class ScoreManager:
             x = '\t'.join([str(i) for i in s])
             print(x)
 
-if __name__ == '__main__':
-    a = ScoreManager()
+class ScoreViewer:
+    def __init__(self):
+        self.score_manager = ScoreManager()
+        #with open('settings.json') as f:
+        #    self.settings = json.load(f)
 
-    b = a.disp_diff_best('DP12')
+    def ico_path(self, relative_path):
+        try:
+            base_path = sys._MEIPASS
+        except Exception:
+            base_path = os.path.abspath(".")
+        return os.path.join(base_path, relative_path)
+    
+    def export_csv(self):
+        savedir = filedialog.askdirectory()
+        ret = []
+        if savedir != '':
+            for mode in ('SP', 'DP', 'DB'):
+                for lv in range(1, 13):
+                    lvlist = self.score_manager.get_diff_best(f"{mode}{lv}")
+                    for tmp in lvlist:
+                        # tmpの形式:get_diff_scores_bestの形式なのでここで整形しておく
+                        tmp.insert(0, f'☆{lv}')
+                        if tmp[5] == 9999:
+                            tmp[5] = ''
+                        # score, bp, notesの順なので、これをscore, score_rate, bpに書き換える
+                        bp = tmp[5]
+                        tmp[5] = f"{tmp[4]*100/(tmp[6]*2):.1f}"
+                        tmp[6] = bp
+                        date = tmp[-1].split('-')
+                        tmp[-1] = f"{date[0]}/{date[1]}/{date[2]} {date[3]}:{date[4]}"
+                        ret.append(tmp)
+            outfile = f'{savedir}/inf_score.csv'
+            try:
+                with open(outfile, 'w') as f:
+                    w = csv.writer(f, lineterminator="\n")
+                    w.writerow(['LV', 'Title', 'mode', 'Lamp', 'Score', '(rate)', 'BP', 'Opt(best score)', 'Opt(min bp)', 'Last Played'])
+                    for s in ret:
+                        w.writerow(s)
+                sg.popup(f'CSVエクスポート完了.\n --> {outfile}')
+            except Exception as e:
+                sg.popup(f'error!!\n{e}')
+    
+    def gui(self):
+        sg.theme('SystemDefault')
+        header = ['LV', 'Title', 'mode', 'Lamp', 'Score', '(rate)', 'BP', 'Opt(best score)', 'Opt(min bp)', 'Last Played']
+        layout_mode = []
+        for mode in ('sp', 'dp', 'dbx'):
+            layout_mode.append(sg.Radio(mode.upper(), key=f"radio_mode_{mode}", group_id="mode", default=mode=='sp', enable_events=True))
+        layout_lv = [sg.Checkbox('ALL', key='chk_lvall', enable_events=True, default=True)]
+        for i in range(1, 13):
+            layout_lv.append(sg.Checkbox(f'☆{i}', key=f"chk_lv{i}", enable_events=True, default=True))
+        layout = [
+            layout_mode,
+            layout_lv,
+            [sg.Text('search:'), sg.Input('', key='txt_search', enable_events=True), sg.Button('CSVにエクスポート', key='btn_export', enable_events=True, tooltip='プレーデータをcsvに保存します。\nSP/DP/DoubleBattleのデータを全て1ファイルに書き出します。')],
+            [sg.Table([], key='table', headings=header
+                      , font=(None, 16)
+                      , vertical_scroll_only=False
+                      , auto_size_columns=False
+                      , col_widths=[4, 40, 4, 10, 5, 5, 5, 20, 20, 14]
+                      ,background_color='#ffffff'
+                      ,alternating_row_color='#eeeeee'
+                      , justification='left', size=(1,10))
+            ],
+        ]
+        ico=self.ico_path('icon.ico')
+        self.window = sg.Window("INFINITAS Score Viewer", layout, resizable=True, return_keyboard_events=True, finalize=True, enable_close_attempted_event=True, icon=ico, size=(800,600))
+        self.window['table'].expand(expand_x=True, expand_y=True)
+        self.update_table()
+
+    def update_table(self):
+        mode = 'SP'
+        if self.window['radio_mode_dp'].get():
+            mode = 'DP'
+        elif self.window['radio_mode_dbx'].get():
+            mode = 'DB'
+        dat = []
+        for i in range(1, 13):
+            if self.window[f"chk_lv{i}"].get():
+                lvs = self.score_manager.get_diff_best(f"{mode}{i}")
+                for tmp in lvs:
+                    # tmpの形式:get_diff_scores_bestの形式なのでここで整形しておく
+                    tmp.insert(0, f'☆{i}')
+                    if tmp[5] == 9999:
+                        tmp[5] = ''
+                    # score, bp, notesの順なので、これをscore, score_rate, bpに書き換える
+                    bp = tmp[5]
+                    tmp[5] = f"{tmp[4]*100/(tmp[6]*2):.1f}"
+                    tmp[6] = bp
+                    date = tmp[-1].split('-')
+                    tmp[-1] = f"{date[0]}/{date[1]}/{date[2]} {date[3]}:{date[4]}"
+                    to_push = True
+                    if self.window['txt_search'].get().strip() != '':
+                        for search_word in self.window['txt_search'].get().strip().split(' '):
+                            if search_word.lower() not in tmp[1].lower():
+                                to_push = False
+                    if to_push:
+                        dat.append(tmp)
+        self.window['table'].update(dat)
+
+
+    def main(self):
+        self.gui()
+
+        while True:
+            ev, val = self.window.read()
+            print(ev, val)
+            if ev in (sg.WIN_CLOSED, 'Escape:27', '-WINDOW CLOSE ATTEMPTED-', 'btn_close_setting', 'btn_close_info'): # 終了処理
+                break
+            elif ev == 'chk_lvall': # ALLボタンの処理
+                for i in range(1,13):
+                    self.window[f"chk_lv{i}"].update(val['chk_lvall'])
+            elif ev == 'btn_export':
+                self.export_csv()
+            self.update_table()
+
+if __name__ == '__main__':
+    c = ScoreManager()
+    #b = c.disp_diff_best('DP12')
+
+    a = ScoreViewer()
+    a.main()
+
