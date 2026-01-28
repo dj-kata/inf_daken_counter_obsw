8a619578150559f3851636ca93035524e4dcf982
katao <k.katasan@gmail.com>
2026-01-28 16:25:17 +0900

    mod: Judgeについて、PostPlayJudgeとPosResultJudgeに分けた

--- src/define.py
+++ src/define.py
@@ -34,10 +34,42 @@ class PosIsPlay:
             y = 872
         return (x, y, x+74, y+16)
 
-class PosJudge:
-    '''詳細判定部分の座標を返すクラス。'''
+class PosPlayJudge:
+    '''プレー画面における詳細判定部分の座標を返すクラス。'''
     @classmethod
     def get(self, mode:play_mode):
+        '''モードに対する判定部分の位置を返す。img.crop()でそのまま使えるようにtupleで返す。'''
+        x = 0
+        y = 0
+        if mode == play_mode.sp_1p_l:
+            x = 632
+            y = 969
+        elif mode == play_mode.sp_1p_r:
+            x = 1049
+            y = 969
+        elif mode == play_mode.sp_1p_nograph:
+            x = 678
+            y = 969
+        elif mode == play_mode.sp_2p_l:
+            x = 852
+            y = 969
+        elif mode == play_mode.sp_2p_r:
+            x = 1269
+            y = 969
+        elif mode == play_mode.sp_2p_nograph:
+            x = 1223
+            y = 969
+        elif mode == play_mode.dp_l:
+            x = 262
+            y = 898
+        elif mode == play_mode.dp_r:
+            x = 1643
+            y = 898
+        return (x, y, x+53, y+91)
+
+class PosResultJudge:
+    '''Result画面における詳細判定部分の座標を返すクラス。'''
+    def get(self, mode:play_side):
         '''モードに対する判定部分の位置を返す。img.crop()でそのまま使えるようにtupleで返す。'''
         x = 0
         y = 0
