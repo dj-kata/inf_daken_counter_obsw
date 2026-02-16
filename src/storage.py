6964195d80378b2115dbbfccf11b522e1412dc8d
dj-kata <k.katasan@gmail.com>
2026-02-16 19:06:07 +0900

    resources updated

--- src/storage.py
+++ src/storage.py
@@ -8,7 +8,10 @@ from json import loads,dumps
 from uuid import uuid1
 from datetime import datetime,timezone
 from threading import Thread
+# from logging import getLogger
 
+# logger = getLogger().getChild('storage')
+# logger.debug('loaded storage.py')
 from src.logger import get_logger
 logger = get_logger(__name__)
 
@@ -19,13 +22,11 @@ sys.path.append('infnotebook')
 from define import define
 from result import Result
 
-BUCKET_NAMES = {
-    'informations': 'bucket-inf-notebook-informations',
-    'details': 'bucket-inf-notebook-details',
-    'musicselect': 'bucket-inf-notebook-musicselect',
-    'resources': 'bucket-inf-notebook-resources',
-    'discordwebhooks': 'bucket-inf-notebook-discordwebhook',
-}
+bucket_name_informations = 'bucket-inf-notebook-informations'
+bucket_name_details = 'bucket-inf-notebook-details'
+bucket_name_musicselect = 'bucket-inf-notebook-musicselect'
+bucket_name_resources = 'bucket-inf-notebook-resources'
+bucket_name_discordwebhooks = 'bucket-inf-notebook-discordwebhook'
 
 informations_dirname = 'informations'
 details_dirname = 'details'
@@ -56,51 +57,81 @@ musicselect_rivals_fillbox = (
 
 class StorageAccessor():
     client = None
+    bucket_informations = None
+    bucket_details = None
+    bucket_musicselect = None
+    bucket_resources = None
+    bucket_discordwebhooks = None
     blob_musics = None
-
-    def __init__(self):
-        self._buckets = {}
 
     def connect_client(self):
         if self.client is not None:
             return
-
+        
         if service_account_info is None:
             logger.info('no define service_account_info')
             return
-
+        
         self.client = storage.Client.from_service_account_info(service_account_info)
         logger.debug('connect client')
 
-    def _get_bucket(self, name: str):
-        """バケットを取得し、未接続なら接続する"""
-        if name in self._buckets and self._buckets[name] is not None:
-            return self._buckets[name]
-
+    def connect_bucket_informations(self):
         if self.client is None:
             self.connect_client()
         if self.client is None:
-            return None
-
+            return
+        
         try:
-            self._buckets[name] = self.client.get_bucket(BUCKET_NAMES[name])
-            logger.debug(f'connect bucket {name}')
+            self.bucket_informations = self.client.get_bucket(bucket_name_informations)
+            logger.debug('connect bucket informations')
         except Exception as ex:
             logger.exception(ex)
-            return None
 
-        return self._buckets[name]
-
-    def _upload_image_to_bucket(self, bucket_name: str, object_name: str, image):
-        """指定バケットに画像をアップロードする"""
-        bucket = self._get_bucket(bucket_name)
-        if bucket is None:
+    def connect_bucket_details(self):
+        if self.client is None:
+            self.connect_client()
+        if self.client is None:
             return
-
+        
         try:
-            blob = bucket.blob(object_name)
-            self.upload_image(blob, image)
-            logger.debug(f'upload {bucket_name} image {object_name}')
+            self.bucket_details = self.client.get_bucket(bucket_name_details)
+            logger.debug('connect bucket details')
+        except Exception as ex:
+            logger.exception(ex)
+    
+    def connect_bucket_musicselect(self):
+        if self.client is None:
+            self.connect_client()
+        if self.client is None:
+            return
+        
+        try:
+            self.bucket_musicselect = self.client.get_bucket(bucket_name_musicselect)
+            logger.debug('connect bucket musicselect')
+        except Exception as ex:
+            logger.exception(ex)
+    
+    def connect_bucket_resources(self):
+        if self.client is None:
+            self.connect_client()
+        if self.client is None:
+            return
+        
+        try:
+            self.bucket_resources = self.client.get_bucket(bucket_name_resources)
+            logger.debug('connect bucket resources')
+        except Exception as ex:
+            logger.exception(ex)
+
+    def connect_bucket_discordwebhooks(self):
+        if self.client is None:
+            self.connect_client()
+        if self.client is None:
+            return
+        
+        try:
+            self.bucket_discordwebhooks = self.client.get_bucket(bucket_name_discordwebhooks)
+            logger.debug('connect bucket discordwebhooks')
         except Exception as ex:
             logger.exception(ex)
 
@@ -109,6 +140,45 @@ class StorageAccessor():
         image.save(bytes, 'PNG')
         blob.upload_from_file(bytes, True)
         bytes.close()
+
+    def upload_informations(self, object_name, image):
+        if self.bucket_informations is None:
+            self.connect_bucket_informations()
+        if self.bucket_informations is None:
+            return
+
+        try:
+            blob = self.bucket_informations.blob(object_name)
+            self.upload_image(blob, image)
+            logger.debug(f'upload information image {object_name}')
+        except Exception as ex:
+            logger.exception(ex)
+
+    def upload_details(self, object_name, image):
+        if self.bucket_details is None:
+            self.connect_bucket_details()
+        if self.bucket_details is None:
+            return
+
+        try:
+            blob = self.bucket_details.blob(object_name)
+            self.upload_image(blob, image)
+            logger.debug(f'upload details image {object_name}')
+        except Exception as ex:
+            logger.exception(ex)
+
+    def upload_musicselect(self, object_name, image):
+        if self.bucket_musicselect is None:
+            self.connect_bucket_musicselect()
+        if self.bucket_musicselect is None:
+            return
+
+        try:
+            blob = self.bucket_musicselect.blob(object_name)
+            self.upload_image(blob, image)
+            logger.debug(f'upload musicselect image {object_name}')
+        except Exception as ex:
+            logger.exception(ex)
 
     def start_uploadcollection(self, result: Result, image: Image.Image, force: bool):
         '''収集画像をアップロードする
@@ -124,7 +194,7 @@ class StorageAccessor():
         self.connect_client()
         if self.client is None:
             return
-
+        
         object_name = f'{uuid1()}.png'
 
         informations_trim = force
@@ -158,16 +228,16 @@ class StorageAccessor():
 
         if informations_trim:
             trim = image.crop(define.informations_trimarea)
-            Thread(target=self._upload_image_to_bucket, args=('informations', object_name, trim,)).start()
+            Thread(target=self.upload_informations, args=(object_name, trim,)).start()
         if details_trim:
             play_side = result.play_side
             trim = image.crop(define.details_trimarea[play_side])
             image_draw = ImageDraw.Draw(trim)
             image_draw.rectangle(result_rivalname_fillbox, fill=0)
-            Thread(target=self._upload_image_to_bucket, args=('details', object_name, trim,)).start()
-
+            Thread(target=self.upload_details, args=(object_name, trim,)).start()
+        
         return informations_trim, details_trim
-
+    
     def start_uploadmusicselect(self, image):
         '''選曲画面の収集画像をアップロードする
 
@@ -177,48 +247,51 @@ class StorageAccessor():
         self.connect_client()
         if self.client is None:
             return
-
+        
         object_name = f'{uuid1()}.png'
 
         trim = image.crop(define.musicselect_trimarea)
         image_draw = ImageDraw.Draw(trim)
         image_draw.rectangle(musicselect_rivals_fillbox, fill=0)
-        Thread(target=self._upload_image_to_bucket, args=('musicselect', object_name, trim,)).start()
-
+        Thread(target=self.upload_musicselect, args=(object_name, trim,)).start()
+    
     def upload_resource(self, resourcename, targetfilepath):
-        bucket = self._get_bucket('resources')
-        if bucket is None:
+        if self.bucket_resources is None:
+            self.connect_bucket_resources()
+        if self.bucket_resources is None:
             return
 
         try:
-            blob = bucket.blob(resourcename)
+            blob = self.bucket_resources.blob(resourcename)
             blob.upload_from_filename(targetfilepath)
             logger.debug(f'upload resource {targetfilepath}')
         except Exception as ex:
             logger.exception(ex)
-
+    
     def get_resource_timestamp(self, resourcename):
-        bucket = self._get_bucket('resources')
-        if bucket is None:
+        if self.bucket_resources is None:
+            self.connect_bucket_resources()
+        if self.bucket_resources is None:
             return None
 
         try:
-            blob = bucket.get_blob(resourcename)
+            blob = self.bucket_resources.get_blob(resourcename)
             if blob is None:
                 return None
-
+            
             return str(blob.updated)
         except Exception as ex:
             logger.exception(ex)
-
+        
         return None
-
+    
     def download_resource(self, resourcename, targetfilepath):
-        bucket = self._get_bucket('resources')
-        if bucket is None:
+        if self.bucket_resources is None:
+            self.connect_bucket_resources()
+        if self.bucket_resources is None:
             return False
-
-        blob = bucket.get_blob(resourcename)
+        
+        blob = self.bucket_resources.get_blob(resourcename)
 
         try:
             blob.download_to_filename(targetfilepath)
@@ -226,17 +299,18 @@ class StorageAccessor():
         except Exception as ex:
             logger.exception(ex)
             return False
-
+        
         return True
-
+    
     def download_discordwebhooks(self) -> dict[dict] | None:
-        bucket = self._get_bucket('discordwebhooks')
-        if bucket is None:
+        if self.bucket_discordwebhooks is None:
+            self.connect_bucket_discordwebhooks()
+        if self.bucket_discordwebhooks is None:
             return None
-
+        
         list = {}
 
-        blobs = self.client.list_blobs(BUCKET_NAMES['discordwebhooks'])
+        blobs = self.client.list_blobs(bucket_name_discordwebhooks)
         for blob in blobs:
             blob: Blob = blob
             content = loads(blob.download_as_string())
@@ -250,10 +324,10 @@ class StorageAccessor():
                 # 終了日時を過ぎたファイルは削除する
                 try:
                     blob.delete()
-                except Exception:
+                except Exception as ex:
                     pass
 
-
+        
         return list
 
     def upload_discordwebhook(self, filename: str, value: dict) -> bool:
@@ -266,12 +340,13 @@ class StorageAccessor():
         Returns:
             bool: アップロードの成功
         '''
-        bucket = self._get_bucket('discordwebhooks')
-        if bucket is None:
+        if self.bucket_discordwebhooks is None:
+            self.connect_bucket_discordwebhooks()
+        if self.bucket_discordwebhooks is None:
             return False
-
+        
         try:
-            blob = bucket.blob(filename)
+            blob = self.bucket_discordwebhooks.blob(filename)
             blob.upload_from_string(dumps(value))
             logger.debug(f'upload discordwebhooks {filename}')
         except Exception as ex:
@@ -279,11 +354,11 @@ class StorageAccessor():
             return False
 
         return True
-
+    
     def save_image(self, basepath, blob: Blob):
         if not exists(basepath):
             mkdir(basepath)
-
+        
         image_bytes = blob.download_as_bytes()
         image = Image.open(io.BytesIO(image_bytes))
         filepath = join(basepath, blob.name)
@@ -298,18 +373,27 @@ class StorageAccessor():
         if not exists(basedir):
             mkdir(basedir)
 
-        targets = [
-            (BUCKET_NAMES['informations'], join(basedir, informations_dirname)),
-            (BUCKET_NAMES['details'], join(basedir, details_dirname)),
-            (BUCKET_NAMES['musicselect'], join(basedir, musicselect_dirname)),
-        ]
+        informations_dirpath = join(basedir, informations_dirname)
+        details_dirpath = join(basedir, details_dirname)
+        musicselect_dirpath = join(basedir, musicselect_dirname)
 
         count = 0
-        for bucket_name, dirpath in targets:
-            blobs = self.client.list_blobs(bucket_name)
-            for blob in blobs:
-                self.save_image(dirpath, blob)
-                blob.delete()
-                count += 1
+        blobs = self.client.list_blobs(bucket_name_informations)
+        for blob in blobs:
+            self.save_image(informations_dirpath, blob)
+            blob.delete()
+            count += 1
+
+        blobs = self.client.list_blobs(bucket_name_details)
+        for blob in blobs:
+            self.save_image(details_dirpath, blob)
+            blob.delete()
+            count += 1
+
+        blobs = self.client.list_blobs(bucket_name_musicselect)
+        for blob in blobs:
+            self.save_image(musicselect_dirpath, blob)
+            blob.delete()
+            count += 1
 
         print(f'download count: {count}')
