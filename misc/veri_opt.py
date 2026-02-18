3186548c40fb92831bde79c219ac49421984fbaf
dj-kata <k.katasan@gmail.com>
2026-02-18 15:14:44 +0900

    オプション検証用

---
+++ misc/veri_opt.py
@@ -0,0 +1,28 @@
+import sys
+import pickle
+import os
+from PIL import Image
+import numpy as np
+import imagehash
+import glob
+
+from src.screen_reader import ScreenReader
+from src.logger import get_logger
+from src.result import *
+from src.result_database import ResultDatabase
+from src.classes import *
+from src.config import Config
+from src.funcs import *
+logger = get_logger('exe_recog')
+
+if __name__ == '__main__':
+    logger.info('start')
+    config = Config()
+    # rdb = ResultDatabase(config = config)
+    reader = ScreenReader()
+    for f in glob.glob('debug/option/*.png'):
+        logger.info(f'file={f}')
+        reader.update_screen_from_file(f)
+        if reader.is_option():
+            opt = reader.read_option_screen()
+            print(opt)
